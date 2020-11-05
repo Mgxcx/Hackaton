b9e02cebbdacb4fe0af95d6d92e246b92eb68ae3
Margaux Chevreux <margaux.chevreux@orange.fr>
2020-11-05 11:39:41 +0100

    Adding User Model

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+var userModel = require("../models/users");
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
